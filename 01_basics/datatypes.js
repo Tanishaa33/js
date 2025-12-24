85056415a914e73b5a7952cefc35748d401d4229
Tanishaa33 <rathoretanisha9425@gmail.com>
2025-12-24 02:01:50 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+ let name = jonh
+ let age = 18
+ let isLognedIn = true
+ let state = null
+
+
+ //number - 2 to power 52
+ //bigint - 
+ //string - ""
+ //boolean - true/false
+ //null - standalone value
+ //undefined - 
+ //symbol - umique
