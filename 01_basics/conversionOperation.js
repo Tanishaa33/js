880c68d8d08e814b16b40319c62ee3bc93cfb115
Tanishaa33 <rathoretanisha9425@gmail.com>
2026-02-03 13:23:19 +0000

    variable and conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+/*let score = "33";
+console.log(typeof score);                  //"33" = 33
+console.log(typeof (score));                //"33abc" = NaN
+                                            //true = 1;  false = 0;
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+
+/*let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);      
+console.log(typeof booleanisLoggedIn);               // "" = false;
+console.log(booleanisLoggedIn);*/                    // "abc" = 1
+                                                     // 1 = true ; 0 = false;
+
+
+let number = 19;
+console.log(typeof number);
+
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId =  144553
+let accountEmail = "Tanu@gmail.com"
+var accountpassword = "12345"
+accountcity = "Jaipur"
+
+accountId = 112233
+
+console.log(accountId);
