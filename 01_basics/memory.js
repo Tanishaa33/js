e52806c93e6b6f986be0042bbebbcc969a331a66
Tanishaa33 <rathoretanisha9425@gmail.com>
2026-03-08 18:22:18 +0000

    memory & string

---
+++ 01_basics/memory.js
@@ -0,0 +1,23 @@
+let class1 = "12a";
+
+let anotherclass = "class1";
+anotherclass = "12b"
+
+console.log(class1);
+console.log(anotherclass);
+
+
+
+
+
+let userOne = {
+    email : "abc@gmail.com",
+   upi : "user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "efg@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
