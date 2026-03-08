f6d760447d2fffba61e20c0d0ce63184b32ec19a
Tanishaa33 <rathoretanisha9425@gmail.com>
2026-03-08 17:15:21 +0000

    conversions

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -15,9 +15,43 @@ console.log(booleanisLoggedIn);*/                    // "abc" = 1
                                                      // 1 = true ; 0 = false;
 
 
-let number = 19;
-console.log(typeof number);
+// let number = 19;
+// console.log(typeof number);
 
-let stringNumber = String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(number);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);  
+
+
+// let value = 5;
+// let negValue = -value ;
+// console.log(negValue);
+
+
+// console.log("1" + 2 + 2);   // if 1st value is string => code string
+
+// console.log(1 + 2 + "2");   // if last value is string => operation of starting value and then addition with string 
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+
+
+const arrays  = ["11","22","33"];
+let myObj = {
+    name : "John",
+    age : 20,
+}
+
+const myFunctio = function(){
+    console.log("Hello World");
+}
